036c47914c886248d515bf4ef771e0e22116ab02
pietr <pietrokucharski@hotmail.com>
2025-04-28 23:58:25 -0300

    Create Service layer, component registration

--- src/main/java/com/pietrokucharski/spring_project/resources/UserResources.java
+++ src/main/java/com/pietrokucharski/spring_project/resources/UserResources.java
@@ -1,18 +1,33 @@
 package com.pietrokucharski.spring_project.resources;
 
 import com.pietrokucharski.spring_project.entities.User;
+import com.pietrokucharski.spring_project.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping
-    public ResponseEntity<User> findAll() {
-        User user = new User(1L, "Pietro", "pietro@email.com", "123456789", "1234");
+    public ResponseEntity<List<User>> findAll() {
+        List<User> list = service.findAll();
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id) {
+        User user = service.findById(id);
         return ResponseEntity.ok().body(user);
     }
+
 }
